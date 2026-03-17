7b3b081935bd7a73df347913066ecc419d6d2b56
annaluizacarvalhaes-star <annaluizacarvalhaes@gmail.com>
2026-03-17 11:41:14 -0300

    aula 02

---
+++ Aula02-variaveis-operadores/Aula02a-variaveis.py
@@ -0,0 +1,32 @@
+print("Olá mundo")
+
+print(7+4)
+print ("7"+"4") #CONCATENAÇÃO DE STRINGS
+
+# VARIÁVEIS
+nome = "anna"
+idade = 18
+peso = 60
+
+print(nome,  idade,  peso)
+
+print(f"Oiii {nome}")
+
+# INPUT - SIMULA FORMS NO CMD
+nome = input("Digite o seu nome: ")
+idade = int(input("Digite a sua idade: ")) #Com o int retorna numeros
+peso = input("Digite o seu peso: ")
+
+print(nome,  idade,  peso)
+
+
+#Atividade 1
+nome = input("Digite o seu nome: ")
+print(f"Boas vindas {nome}")
+
+#Atividade 2
+Dia = input("Qual o dia do seu aniversario? ")
+Mes = input("qual o mês do seu aniversário? ")
+Ano = input("qual o ano que você nasceu? ")
+print("\n")
+print(f"{Dia} / {Mes} / {Ano}")
---
+++ Aula02-variaveis-operadores/Desafios-aula02.py
@@ -0,0 +1,15 @@
+#Atividade 1
+nome = input("Digite o seu nome: ")
+print(f"Boas vindas {nome}")
+
+#Atividade 2
+Dia = input("Qual o dia do seu aniversario? ")
+Mes = input("qual o mês do seu aniversário? ")
+Ano = input("qual o ano que você nasceu? ")
+print("\n")
+print(f"{Dia} / {Mes} / {Ano}")
+
+#Atividade 3
+a = int(input("Digite um numero: "))
+b = int(input("Digite outro numero: "))
+print("a+b")
